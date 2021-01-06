a756c32f54ddf9920d30d8cabe37e2b6d0d1fc40
username <eamil@qq.com>
2021-01-06 17:08:03 +0800

    任务23：0401-单例模式（访问GameFacade）（完成）

---
+++ RedAlertProject/Assets/Scripts/GameFacade.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+//外观模式
+public class GameFacade
+{
+    private static GameFacade _instance = new GameFacade();
+
+    private bool mIsGameOver = false;//游戏是否结束
+
+    public static GameFacade Insance { get { return _instance; } }
+
+
+    public bool isGameOver { get { return mIsGameOver; } }
+
+    private GameFacade()
+    {
+
+    }
+
+    public void Init()
+    {
+
+    }
+
+
+    public void Update()
+    {
+
+    }
+
+    public void Release()
+    {
+
+    }
+
+
+}
+
--- RedAlertProject/Assets/Scripts/SceneState/BattleState.cs
+++ RedAlertProject/Assets/Scripts/SceneState/BattleState.cs
@@ -9,5 +9,25 @@ public class BattleState:ISceneState
     {
 
     }
+
+    //private GameFacade mFacade;改成单例模式进行访问，此变量弃用
+    //兵营 关卡 角色管理 行动力 成就系统。。。
+
+    public override void StateStart()
+    {
+        GameFacade.Insance.Init();//初始化
+    }
+    public override void StateEnd()
+    {
+        GameFacade.Insance.Release();//结束后进行的操作
+    }
+    public override void StateUpdata()
+    {
+        if (GameFacade.Insance.isGameOver)
+        {
+            mController.SetState(new MainMenuState(mController));
+        }
+        GameFacade.Insance.Update();
+    }
 }
 
